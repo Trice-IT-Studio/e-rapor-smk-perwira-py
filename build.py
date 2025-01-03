99fda6c361a4abf56dfd8770a4b8daa56074b5da
zagdiablo <zagdiablo@github.com>
2025-01-03 19:54:45 +0700

    test build

--- build.py
+++ build.py
@@ -1,15 +1,18 @@
 from main import WORKING_DIR
 import os
 
-STATIC_FOLDER = f'{WORKING_DIR}/src/static'
-TEMPLATE_FOLDER = f'{WORKING_DIR}/src/templates'
+STATIC_FOLDER = f"{WORKING_DIR}/src/static"
+TEMPLATE_FOLDER = f"{WORKING_DIR}/src/templates"
 
-DIST_FOLDER_NAME = 'erapor_release'
-DIST_DEST = f'{WORKING_DIR}../../{DIST_FOLDER_NAME}'
+DIST_FOLDER_NAME = "erapor_release"
+DIST_DEST = f"{WORKING_DIR}../../{DIST_FOLDER_NAME}"
 
 # RUN BUILD COMMAND
 try:
-    os.system(f'pyinstaller --add-data "{STATIC_FOLDER}:src/static" --add-data "{TEMPLATE_FOLDER}:src/templates" main.py --distpath {DIST_DEST}')
+    # development build
+    os.system(
+        f'pyinstaller --onefile --add-data "{STATIC_FOLDER}:src/testing_modules" --add-data "{STATIC_FOLDER}:src/modules" --add-data "{STATIC_FOLDER}:src/static" --add-data "{TEMPLATE_FOLDER}:src/templates" main.py --distpath {DIST_DEST}'
+    )
 except Exception:
-    print('Something went wrong.')
+    print("Something went wrong.")
     exit()
--- main.py
+++ main.py
@@ -6,9 +6,9 @@ WORKING_DIR = os.path.dirname(os.path.abspath(__file__))
 app = App(WORKING_DIR)
 
 if __name__ == "__main__":
-    # try:
-    #     webview.create_window('E-Rapor SMK Perwira', app, maximized=True)
-    #     webview.start(gui='qt')
-    # except webview.errors.WebViewException as e:
-    #     print('Error: ', e)
-    app.run() # TODO delete for production
+    try:
+        webview.create_window("E-Rapor SMK Perwira", app, maximized=True)
+        webview.start(gui="qt")
+    except webview.errors.WebViewException as e:
+        print("Error: ", e)
+    # app.run() # TODO delete for production
